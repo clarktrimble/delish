1d9695b3f593e74794024f62ca9bd2f4ec82843f
Clark Trimble <clarktrimble@users.noreply.github.com>
2025-07-16 07:36:30 +0300

    write http from respond

--- respond/respond.go
+++ respond/respond.go
@@ -4,10 +4,13 @@ package respond
 import (
 	"context"
 	"encoding/json"
+	"html/template"
 	"net/http"
 
 	"github.com/pkg/errors"
 )
+
+// Todo: think about respond as it's own tiny module
 
 // Respond provides convinience methods when responding to a json request.
 type Respond struct {
@@ -28,14 +31,14 @@ func New(writer http.ResponseWriter, lgr logger) *Respond {
 // Ok responds with 200 ok.
 func (rp *Respond) Ok(ctx context.Context) {
 
-	rp.header(200)
+	rp.jsonHeader(200)
 	rp.Write(ctx, []byte(`{"status":"ok"}`))
 }
 
 // NotOk logs an error and responds with it.
 func (rp *Respond) NotOk(ctx context.Context, code int, err error) {
 
-	rp.header(code)
+	rp.jsonHeader(code)
 
 	rp.Logger.Error(ctx, "returning error to client", err)
 	rp.WriteObjects(ctx, map[string]any{"error": err.Error()})
@@ -56,26 +59,56 @@ func (rp *Respond) GoNoGo(ctx context.Context, code int, err error) {
 // NotFound responds with 404 not found.
 func (rp *Respond) NotFound(ctx context.Context) {
 
-	rp.header(404)
+	rp.jsonHeader(404)
 	rp.Write(ctx, []byte(`{"not":"found"}`))
 }
 
 // WriteObjects responds with marshalled objects by key.
 func (rp *Respond) WriteObjects(ctx context.Context, objects map[string]any) {
 
-	rp.header(0)
+	rp.jsonHeader(0)
 
 	data, err := json.Marshal(objects)
 	if err != nil {
 		err = errors.Wrapf(err, "somehow failed to encode: %#v", objects)
 		rp.Logger.Error(ctx, "failed to encode response", err)
 
-		rp.header(500)
+		rp.jsonHeader(500)
 		rp.Write(ctx, []byte(`{"error": "failed to encode response"}`))
 		return
 	}
 
 	rp.Write(ctx, data)
+}
+
+// WriteObject responds with marshalled object.
+func (rp *Respond) WriteObject(ctx context.Context, obj any) {
+
+	// Todo: no need for "Objects" version, yeah?
+	//       in any case, unit!
+
+	rp.jsonHeader(0)
+
+	data, err := json.Marshal(obj)
+	if err != nil {
+		err = errors.Wrapf(err, "somehow failed to encode: %#v", obj)
+		rp.Logger.Error(ctx, "failed to encode response", err)
+
+		rp.jsonHeader(500)
+		rp.Write(ctx, []byte(`{"error": "failed to encode response"}`))
+		return
+	}
+
+	rp.Write(ctx, data)
+}
+
+func (rp *Respond) WriteHtml(ctx context.Context, content template.HTML) {
+
+	// Todo: want string and sanitize here?
+	//       in any case, unit!
+
+	rp.Writer.Header().Set("Content-Type", "text/html")
+	rp.Write(ctx, []byte(content))
 }
 
 // Write respondes with arbitrary data, logging if error.
@@ -93,10 +126,11 @@ func (rp *Respond) Write(ctx context.Context, data []byte) {
 // unexported
 
 type logger interface {
+	// Todo: public and look at godoc ffs :)
 	Error(ctx context.Context, msg string, err error, kv ...any)
 }
 
-func (rp *Respond) header(code int) {
+func (rp *Respond) jsonHeader(code int) {
 
 	rp.Writer.Header().Set("content-type", "application/json")
 	if code != 0 {
